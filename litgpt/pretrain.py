diabled fabric recompile to fix precision issue related to rope bufferes

litgpt/pretrain.py
@@ -209,7 +209,7 @@ def main(
     fabric.print(f"Total parameters: {num_parameters(model):,}")
 
     model = torch.compile(model)
-    model = fabric.setup(model)
+    model = fabric.setup(model, _reapply_compile=False)
 
     extra_kwargs = {"fused": fabric.device.type == "cuda"}
     optimizer = instantiate_torch_optimizer(optimizer, model.parameters(), **extra_kwargs)